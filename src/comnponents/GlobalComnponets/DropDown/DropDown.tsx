e0ad54966584ded24e9cd0f769c664c0d15456bf
MrHasanovAmin <lolotron022@gmail.com>
2024-04-20 09:07:19 +0400

    finished dropDown component

--- src/comnponents/GlobalComnponets/DropDown/DropDown.tsx
+++ src/comnponents/GlobalComnponets/DropDown/DropDown.tsx
@@ -2,28 +2,34 @@ import { ReactNode, useEffect, useState } from 'react'
 import c from './DropDown.module.scss'
 import classNames from 'classnames'
 
-function DropDown({ dropDownAnimationState, children }: { children: ReactNode, dropDownAnimationState: boolean }) {
-  const [dropDownState, setDropDownState] = useState(true);
+function DropDown({ dropDownAnimationState, children, transitionTime = 400 }: componentProps) {
+  const [dropDownState, setDropDownState] = useState(false);
   const contentClass = classNames(c.content, { [c.activeDropDown]: dropDownAnimationState })
-  const handleTransitionEnd = () => {
-    if (!dropDownAnimationState) {
-      setDropDownState(false)
-    }
-  }
 
   useEffect(() => {
     if (dropDownAnimationState) {
       setDropDownState(true);
-    }
+    } else {
+      setTimeout(() => setDropDownState(false), transitionTime)
+    };
   }, [dropDownAnimationState])
 
-  return dropDownState && (
-    <div className={c.component}>
-      <div className={contentClass} onTransitionEnd={handleTransitionEnd}>
-        {children}
-      </div>
+  return (
+    <div className={c.component} style={{ "--transitionTime": transitionTime + "ms" } as React.CSSProperties}>
+      {
+        dropDownState &&
+        <div className={contentClass}>
+          {children}
+        </div>
+      }
     </div>
   )
 }
 
 export default DropDown
+
+interface componentProps {
+  children: ReactNode,
+  dropDownAnimationState: boolean,
+  transitionTime?: number
+}
